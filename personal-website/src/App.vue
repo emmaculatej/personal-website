efedea2cbea5bc353d5a368a0d185d030b5a18ff
Emma Jeyarajah <emmaculate.jeyarajah@hotmail.com>
2019-06-18 18:07:28 +0100

    Remove HelloWorld Template. Add basic layout

--- personal-website/src/App.vue
+++ personal-website/src/App.vue
@@ -1,28 +1,44 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <homepage name="Emmaculate Jeyarajah"/>
+    <contact-me />
+    <personal-description />
+    <experience />
+    <c-v />
+    <development-portfolio />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Homepage from './components/HomePage.vue'
+import ContactMe from './components/ContactMe.vue'
+import PersonalDescription from './components/PersonalDescription.vue'
+import Experience from './components/Experience.vue'
+import CV from './components/CV.vue'
+import DevelopmentPortfolio from './components/DevelopmentPortfolio.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Homepage,
+    ContactMe,
+    PersonalDescription,
+    Experience,
+    CV,
+    DevelopmentPortfolio
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
 }
 </style>
---
+++ personal-website/src/components/ContactMe.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="contact-me">
+        <h1> Contact Me </h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
