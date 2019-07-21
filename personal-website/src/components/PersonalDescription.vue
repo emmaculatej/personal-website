b5a51d3baf3e35300050af812bc5200cc9ba98ed
Emma Jeyarajah <emmaculate.jeyarajah@hotmail.com>
2019-07-21 15:17:39 +0100

    Generic Description from LinkedIn

--- personal-website/src/components/PersonalDescription.vue
+++ personal-website/src/components/PersonalDescription.vue
@@ -3,10 +3,13 @@
     <h1> About me </h1>
     <h4> {{ personalDetails.forename}} {{ personalDetails.surname}} </h4>
     <h6> {{ personalDetails.occupation}} </h6>
-    <img src="../assets/profilepic.jpg" style="width: 50%; height: 50%"/>
-    <p> Here goes some general text about me</p>
-</div>
-    
+    <img src="../assets/profilepic.jpg" style="width: 10%; height: 10%"/>
+    <div>
+    <p> Experienced Developer with a demonstrated history of working in the information technology and services industry. 
+        Skilled in HTML, JavaScript, .Net framework, C#, and Visual Studio. 
+        Strong engineering professional with a Bachelor's degree focused in Computer Science from University of Surrey. </p>
+    </div>
+</div>  
 </template>
 
 <script>
