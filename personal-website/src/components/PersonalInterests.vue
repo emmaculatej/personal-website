cdef3f280fe31525832c74d49ae4a6493fcbd1d3
emmaculatej <Emma.Jeyarajah@DCSLSoftware.com>
2019-07-26 15:40:10 +0100

    Card changes

--- personal-website/src/components/PersonalInterests.vue
+++ personal-website/src/components/PersonalInterests.vue
@@ -3,31 +3,46 @@
     <div class="container" id="personalInterestContainer">
         <h4 class="container interestHeading"> Personal Interests </h4>
             <div class="card-deck">
-                <div class="flip-card">
-                    <div class="flip-card-inner">
-                        <div class="flip-card-front">
-                            <img src="../assets/flower.jpg" alt="Avatar" style="width:100%;height:auto;">
-                        </div>
-                        <div class="flip-card-back">
-                            <h1>John Doe</h1> 
-                            <p>Architect & Engineer</p> 
-                            <p>We love that guy</p>
-                        </div>
+            <div class="card">              
+                <div class="card-body">
+                    <img class="card-img-top" src="../assets/flower.jpg" />
+                    <h5 class="card-title"> heading </h5>
+                    <div class="text-center">
+                        <button class="btn btn-danger" v-on:click="youtube = !youtube"> Learn More </button>
                     </div>
-                </div>
-                 <div class="flip-card">
-                    <div class="flip-card-inner">
-                        <div class="flip-card-front">
-                            <img src="../assets/flower.jpg" alt="Avatar" style="width:100%;height:auto;">
-                        </div>
-                        <div class="flip-card-back">
-                            <h1>John Doe</h1> 
-                            <p>Architect & Engineer</p> 
-                            <p>We love that guy</p>
-                        </div>
+                    <div class="card-text centreText" v-if="youtube == true">
+                        <span > some text that needs to be adapted </span>
+                        <span class="card-text"> some text that needs to be adapted </span>
                     </div>
                 </div>
             </div>
+            <div class="card">
+                <div class="card-body" alt="Card image cap">
+                    <img class="card-img-top" src="../assets/flower.jpg" />
+                    <h5 class="card-title"> heading </h5>
+                    <div class="text-center">
+                        <button class="btn btn-warning" v-on:click="music = !music"> Learn More </button>
+                    </div>
+                    <div class="card-text centreText" v-if="music == true">
+                        <span > some text that needs to be adapted </span>
+                    </div>
+                </div>
+            </div>
+            <div class="card">
+                <div class="card-body" alt="Card image cap">
+                        <img class="card-img-top" src="../assets/flower.jpg" />
+                        <h5 class="card-title"> heading </h5>
+                        <div class="text-center">
+                            <button class="btn btn-success" v-on:click="fitness = !fitness"> Learn More </button>
+                        </div>
+                        <div class="card-text centreText" v-if="fitness == true">
+                            <span > fsdfsdfdsffffffffffffffffffffffffffffffffffffffffffffff
+                                fffffffffffffffffffffff
+                            </span>
+                        </div>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 </template>
@@ -35,7 +50,26 @@
 <script>
 
 export default {
-    name: 'PersonalInterests'
+    name: 'PersonalInterests',
+    data: function() {
+        return{
+            youtube: false,
+            music: false,
+            fitness: false
+        }
+    },
+    methods: {
+        showYoutube: void {
+            youtube: true
+        },
+        showMusic: void {
+            music: true
+        },
+        showFitness: void {
+            fitness: true
+        }
+
+    }
 }
 </script>
 
@@ -58,5 +92,8 @@ export default {
 
 .card-title{
     margin-top: 3%;
+}
+.centreText{
+    text-align: center;
 }
 </style>
